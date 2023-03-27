4d4296115c87873a1b2009dd86f2d89aedb6a665
rozybathrick <rebathrick@umass.edu>
2023-03-27 14:55:11 -0400

    wind

--- sbdo_tracks_home/scripts/distance_calc.R
+++ sbdo_tracks_home/scripts/distance_calc.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(sf)
 library(lubridate)
+install.packages("mapview")
 library(mapview)
+
 
 ##load dataset
 sbdo_tracks<-read_csv("raw_data/9.29.22_fall21_22.csv")
@@ -9,13 +11,13 @@ sbdo_tracks<-read_csv("raw_data/9.29.22_fall21_22.csv")
 ##rename columns, change time format, omit blanks, turn the coordinates into spatial data
 sbdo_sf<-sbdo_tracks %>% 
   select("tag-local-identifier", "timestamp", "location-long", "location-lat", 
-         "Breeding-site", "Capture-year") %>% 
+         "Breeding-site", "Capture-year", "height-above-ellipsoid") %>% 
   rename(id="tag-local-identifier",
          long="location-long",
          lat="location-lat",
          site="Breeding-site",
          year="Capture-year",
-         ) %>% 
+         elevation="height-above-ellipsoid") %>% 
   mutate(timestamp=dmy(timestamp)) %>% 
   na.omit() %>% 
   mutate(Jdate=yday(timestamp)) %>% 
@@ -35,13 +37,16 @@ mapview(cook, zcol="id")
 
 sbdo_distance<-sbdo_sf %>% 
   group_by(id) %>% 
-  mutate(dist_to_next = st_distance(lag(geometry), geometry, by_element = TRUE))
+  mutate(dist_to_next = st_distance(lag(geometry), geometry, by_element = TRUE)) %>% 
+  mutate(timetilnext = difftime(lead(timestamp), timestamp, unit="hours"))
+
 
 ##create a new sheet with the distances between each dep and arrival date
 sbdo_dist_bybird<-sbdo_distance %>% 
   group_by(id) %>% 
   filter(Jfly_dist, Jdep_date:Jarr_date)
 
+##filtering for birds that did 2+ day nonstop flights over ocean
 
 #bail to csv
 write.csv(sbdo_distance, file="sbdo_distance.csv")
--- sbdo_tracks_home/scripts/wind.R
+++ sbdo_tracks_home/scripts/wind.R
@@ -11,50 +11,84 @@ library(raster)
 install.packages("ncdf4")
 library(ncdf4)
 library(rgdal) # package for geospatial analysis
+library(lubridate)
+
+### KING SALMON ####
+getwd()
+ks_wind2<-nc_open("raw_data/ks_wind.nc")
+print(ks_wind2)
+
+attributes(ks_wind2$var)
+attributes(ks_wind2$dim)
+
+
+# Get latitude and longitude with the ncvar_get function and store each into their own object:
+ks_lat <- ncvar_get(ks_wind2, "latitude")
+ks_lon <- ncvar_get(ks_wind2, "longitude")
+time<-ncvar_get(ks_wind2, "time")
+
+## extract wind data
+
+ks_wind_array_u <- ncvar_get(ks_wind2, "u")
+dim(u)
+
+ks_wind_array_v <- ncvar_get(ks_wind2, "v") 
+
+## fix time
+
+time_obs <- as.POSIXct(time*3600, origin = "1900-01-01", tz="GMT")
+dim(time_obs)
+range(time_obs)
+
+## into dataframe
+lonlattime<-as.matrix(expand.grid(lon, lat, time_obs))
+u_wind_array<-as.vector(ks_wind_array_u)
+v_wind_array<-as.vector(ks_wind_array_v)                       
+KS_wind_final<-data.frame(cbind(lonlattime, v_wind_array, u_wind_array))
+head(KS_wind_final)
+
+
+View(KS_wind_final)
+colnames(KS_wind_final) <- c("Long", "Lat", "Timestamp", "v", "u")
+write.csv(KS_wind_final, file="KS_wind_final.csv")
+
+##### BELUGA ######
 
 getwd()
-ks_wind <- raster("raw_data/ks_wind.nc")
-print(ks_wind)
-ks_wind_r <- rotate(ks_wind)
-ks_wind_r
-plot(ks_wind_r)
+bel_wind<-nc_open("raw_data/beluga_wind.nc")
+print(bel_wind)
 
-df_ks<-as.data.frame(ks_wind, xy=TRUE, time=TRUE)
+attributes(bel_wind$var)
+attributes(bel_wind$dim)
 
-### trying something new 
 
-nc_ks_wind <- nc_open('raw_data/ks_wind.nc')
+# Get latitude and longitude with the ncvar_get function and store each into their own object:
+bel_lat <- ncvar_get(bel_wind, "latitude")
+bel_lon <- ncvar_get(bel_wind, "longitude")
+bel_time<-ncvar_get(bel_wind, "time")
 
-# Save the print(nc) dump to a text file
-{
-  sink('ks_wind.txt')
-  print(nc_ks_wind)
-  sink()
-}
+## extract wind data
 
-# save attributes independently 
-lon <- ncvar_get(nc_ks_wind, "longitude")
-lat <- ncvar_get(nc_ks_wind, "latitude", verbose = F)
-t <- ncvar_get(nc_ks_wind, "time")
+bel_array_u <- ncvar_get(bel_wind, "u")
 
-head(lon)
-head(lat)
-head(t)
-dim(lon)
-dim(lat)
-dim(t)
-## create arrays
+bel_array_v <- ncvar_get(bel_wind, "v")
 
-u <- ncvar_get(nc_ks_wind, "u") # store the data in a 3-dimensional array
-dim(u)
-v<-ncvar_get(nc_ks_wind, "v") # store the data in a 3-dimensional array
-dim(v)
+## fix time
 
-## combining into dataframe
+bel_time_obs <- as.POSIXct(bel_time*3600, origin = "1900-01-01", tz="GMT")
+dim(bel_time_obs)
+range(bel_time_obs)
 
-ks_df<-data.frame(lon, lat, t, u, v) #it has so many columns I don't know why
-dim(ks_df)
+## into dataframe
+bel_lonlattime<-as.matrix(expand.grid(bel_lon, bel_lat, bel_time_obs))
+bel_u_wind_array<-as.vector(bel_array_u)
+bel_v_wind_array<-as.vector(bel_array_v)                       
+BELUGA_wind_final<-data.frame(cbind(bel_lonlattime, bel_v_wind_array, bel_u_wind_array))
+head(BELUGA_wind_final)
 
-## 2208 = number of time stamps (24 hours * 30 days * 3 months)
-## 13,251 = number of columns (don't know why there are so mnany. I thought there should be two: V and U)
-## 13251/2208 = 6, so maybe it's giving us 6 values for each hour?
+
+View(BELUGA_wind_final)
+colnames(BELUGA_wind_final) <- c("Long", "Lat", "Timestamp", "v", "u")
+write.csv(BELUGA_wind_final, file="BELUGA_wind_final.csv")
+
+
